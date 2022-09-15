6f24b68197d3fddb6ee2c5fb10c313ccba6032ae
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-09-15 22:42:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ st.image("SCT-LOGO.jpg")
 st.title("\t\t\tPOOL LAB")
 df=pd.read_csv(st.file_uploader('Upload a CSV'))
 st.dataframe(df)
+AN=df[['Unnamed: 3']]
+CT=df[['Unnamed: 4']]
+AN=AN.to_numpy()
+CT=CT.to_numpy()

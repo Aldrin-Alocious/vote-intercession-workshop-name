5de1698bfd2fe340af8289cf0138474cd19538f6
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-09-15 22:33:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 st.image("SCT-LOGO.jpg")
-st.title("poolLAB")
+st.title("\t\t\tPOOL LAB")
 df=pd.read_csv(st.file_uploader('Upload a CSV'))
 st.dataframe(df)

b76d4515a205092b8436c22af76a1760c29d5f98
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-09-15 22:10:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-st.file_uploader('Upload a CSV')
-df=pd.read_csv("et lab report.csv")
+df=pd.read_csv(st.file_uploader('Upload a CSV'))
 st.dataframe(df)

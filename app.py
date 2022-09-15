d0587238f859515ff338fcd9d6070436cc44e363
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-09-15 22:28:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+st.image("SCT-LOGO.jpg")
 df=pd.read_csv(st.file_uploader('Upload a CSV'))
 st.dataframe(df)

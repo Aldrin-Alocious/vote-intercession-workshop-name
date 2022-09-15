728cef16ec1e8c5bc51e565ac1dca929ba145973
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-09-15 22:57:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,6 @@ for i in range(1,np.size(AN)):
 nos=np.array([nAI,nCS,nEC,nME,nMP,nMA,nBT,nCL])
 dep=np.array(['Artificial Intelligence And Machine Learning','Computer Science','Electronics and Communication','Mechanical','Mechanical Production','Mechanical Automobile','Biotechnology','Civil'])
 fig1, ax1 = plt.subplots()
-ax1.pie(nos, labels=dep)
+ax1.pie(nos, labels=dep, autopct='%1.1f%%')
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 st.pyplot(fig1)

6687a20a54f963d6e2a083d6d2202fed45dc11fb
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-09-16 23:05:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -169,5 +169,4 @@ with st.form("my_form"):
   op=st.multiselect('Options', options)
   submitted = st.form_submit_button(label='Submit')
   if submitted:
-    st.write("slider", slider_val, "checkbox", checkbox_val)
-st.write("Outside the form")
+    st.write("Outside the form")

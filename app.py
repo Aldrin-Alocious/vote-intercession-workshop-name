76be2eacc077a75ad4c421c753c370b080cd09fe
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-09-19 00:11:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ with st.form("my_form",clear_on_submit=False):
       st.write(item)
 finished=st.button(label='Finish')
 if finished:
-  for item in st.session_state.items():
-    del st.session_state[item]
+  for key in st.session_state.key():
+    del st.session_state[key]
   st.stop()
   st.write("Failed")

dec5ed3d0286426aed8330fac2021b6b9ffa0052
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-09-19 15:46:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -175,6 +175,13 @@ def spotlist(a,b,c,d,e):
   spotdf['KEAM Rank']=c
   spotdf['Reservation Category']=d
   spotdf['Opted']=e
+def decode(ss,n):
+  y=[];
+  for i in range(1,n-1):
+    x=ss.partition('#')
+    y.append(str(x[0])
+    ss=x[2]
+  return y
 if "appno" not in st.session_state:
   st.session_state.appno=''
 if "name" not in st.session_state:
@@ -204,6 +211,12 @@ with st.form("my_form",clear_on_submit=False):
       st.write(item)
 finished=st.button(label='Finish')
 if finished:
+  a=st.session_state['appno'];
+  b=st.session_state['name'];
+  c=st.session_state['keam'];
+  d=st.session_state['reserve'];
+  e=st.session_state['options'];
+  
   st.session_state.appno=''
   st.session_state.name=''
   st.session_state.keam=''

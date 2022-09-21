451478b450c3a2f33210ebbc634cff172612d6d2
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-09-21 20:11:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ h=np.array(h,ndmin=2)
 for i in range(1,np.size(g)):
   if g[i]=='Yes' or h[i]=='NRI' or h[i]=='TFW-Merit':
     dfz=dfz.drop([i])
+dfz=dfz.reset_index(drop=True)
 indice=[];
 k=dfz[['Unnamed: 8']]
 k=k.to_numpy()

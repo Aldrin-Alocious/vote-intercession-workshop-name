7f8422004e33363fc7dbb03401071d5788f4df49
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-09-25 00:19:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -339,7 +339,6 @@ if finished:
   spotdf['Alloted']=y7
   st.subheader("Spot Application List")
   sdf=spotdf.to_csv().encode('utf-8')
-  #spotdf=spotdf.sort_values('KEAM Rank')
   spotdf=spotdf.reset_index(drop=True)
   st.dataframe(spotdf)
   st.download_button("Press to Download",sdf,"file.csv","text/csv",key='download-csv')

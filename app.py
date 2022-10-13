fb875ed91f671cabfc2e1428ae28401ab3dad7b3
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-10-13 22:46:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.image("SCT-LOGO.jpg")
 st.title("POOL LAB")
 df=pd.read_csv(st.file_uploader('Upload a CSV'))
 st.dataframe(df)
-st.sidebar.header("Search here")
+st.sidebar.header("Search by name")
 st.sidebar.text_input("You can check whether a student is in the list or not")
 dfz=df
 g=dfz[['Unnamed: 8']]

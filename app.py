223bdb46284bef11e3b8b948874560c039a10519
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-10-14 00:25:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ with st.sidebar:
   for i in range(1,np.size(g)):
     if not st.session_state['finder']==KM[i,0]:
       dff=dff.drop([i])
-  st.write(dff.to_string(index=False).split('Unnamed: 8'))
+  st.write(dff)
 nAI=0;nCS=0;nEC=0;nME=0;nMA=0;nBT=0;nCL=0;
 AIE=[];CSE=[];ECE=[];MEE=[];MAE=[];BTE=[];CLE=[];
 for i in range(1,np.size(AN)):

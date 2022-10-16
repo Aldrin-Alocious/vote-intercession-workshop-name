e8c31f2b8acdf3ad39a5c5089e382a563d1c0bb3
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2022-10-17 01:51:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -340,19 +340,6 @@ if finished:
       for j in range(0,pq):
         pr=find1(pp[j],y4[i])
         if pr>0:
-          if y6[i]=='Yes':
-            stp=y2[i]
-            NA=df[['Unnamed: 2']]
-            NA=NA.to_numpy()
-            for k in range(0,np.size(NA)):
-              if NA[k]==stp:
-                dfz1.loc[len(dfz1.index)]=df.loc[k]
-                dfz1=dfz1.reset_index(drop=True)
-                break;
-            y7[i]=pp[j]
-            dfz1=dfz1.drop([pr])
-            dfz1=dfz1.reset_index(drop=True)
-            break;
           y7[i]=pp[j]
           dfz1=dfz1.drop([pr])
           dfz1=dfz1.reset_index(drop=True)
@@ -363,19 +350,6 @@ if finished:
     for j in range(0,pq):
       pr=find2(pp[j],'jump')
       if pr>0:
-        if y6[i]=='Yes':
-          stp=y2[i]
-          NA=df[['Unnamed: 2']]
-          NA=NA.to_numpy()
-          for k in range(0,np.size(NA)):
-            if NA[k]==stp:
-              dfz2.loc[len(dfz2.index)]=df.loc[k]
-              dfz2=dfz2.reset_index(drop=True)
-              break;
-          y8[i]=pp[j]
-          dfz2=dfz2.drop([pr])
-          dfz2=dfz2.reset_index(drop=True)
-          break;
         y8[i]=pp[j]
         dfz2=dfz2.drop([pr])
         dfz2=dfz2.reset_index(drop=True)
@@ -389,37 +363,15 @@ if finished:
       else:
         y9[i]=y8[i]
   for i in range(0,len(y6)):
-      if not y9[i]=='':
-        x=y9[i].partition('-')
-        if x[0]=='AI':
-          a='Artificial Intelligence And Machine Learning'
-        elif x[0]=='CS':
-          a='Computer Science'
-        elif x[0]=='EC':
-          a='Electronics and Communication'
-        elif x[0]=='ME':
-          a='Mechanical'
-        elif x[0]=='MA':
-          a='Mechanical Automobile'
-        elif x[0]=='BT':
-          a='Biotechnology'
-        elif x[0]=='CL':
-          a='Civil'
-        if x[2]=='SM':
-          b='Merit'
-        elif x[2]=='MG':
-          b='Management Quota'
-        l=dfz3[['Unnamed: 4']];
-        l=l.to_numpy();
-        m=dfz3[['Unnamed: 6']];
-        m=m.to_numpy();
-        n=dfz3[['Unnamed: 7']];
-        n=n.to_numpy();
-        for j in range(1,np.size(l)):
-          if n[j]==y4[i]:
-            if l[j]==a and m[j]==b:
-              dfz3=dfz3.drop([j])
-              dfz3=dfz3.reset_index(drop=True)
+    if y6[i]=='Yes':
+      stp=y2[i]
+      NA=df[['Unnamed: 2']]
+      NA=NA.to_numpy()
+      for k in range(0,np.size(NA)):
+        if NA[k]==stp:
+          dfz3.loc[len(dfz3.index)]=df.loc[k]
+          dfz3=dfz2.reset_index(drop=True)
+          break;
   spotdf=pd.DataFrame()
   spotdf['Application Number']=y1
   spotdf['Name']=y2

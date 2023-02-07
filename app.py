2feba12030d502e7eddafe26ef7e11d60775caae
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2023-02-08 00:23:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 st.title('TTC Assignment')
 tab1, tab2 = st.tabs(["Internal Trade", "Communication Services"])
 tab1.subheader("Internal Trade")
-op1=tab1.selectbox('Pick one', ['Whole Sale Trade', 'Retail Trade'])
+op1=tab1.radio('Pick one', ['Whole Sale Trade', 'Retail Trade'])
 if op1=='Whole Sale Trade':
   tab1.image('cd4da6f1-bb09-4e25-a203-48565df9a723.jpg')
 elif op1=='Retail Trade':
   tab1.image('3719326c-f43f-417a-b6f1-1c4fe70ec865.jpg')
 tab2.subheader("Communication Services")
-op2=tab2.selectbox('Pick one', ['Post', 'Mobile', 'Television', 'Dish'])
+op2=tab2.radio('Pick one', ['Post', 'Mobile', 'Television', 'Dish'])
 if op2=='Post':
   tab2.image('52d4f12c-a36b-4346-ab91-531882e74196.jpg')
 elif op2=='Mobile':

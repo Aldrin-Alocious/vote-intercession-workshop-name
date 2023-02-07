4e8d9b5e4b984c3bf75a8c14d542635d7ab7793d
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2023-02-08 00:11:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ op2=st.selectbox('Pick one', ['Post', 'Mobile', 'Television', 'Dish'])
 if op2=='Post':
   tab2.image('52d4f12c-a36b-4346-ab91-531882e74196.jpg')
 elif op2=='Mobile':
-  tab2.image('3719326c-f43f-417a-b6f1-1c4fe70ec865.jpg')
+  tab2.image('53e1e72b-4c70-459a-8092-0ce2a2e18e10.jpg')
 elif op2=='Television':
   tab2.image('40b399fd-bf64-49b8-9cc9-edb546e6405a.jpg')
 elif op2=='Dish':

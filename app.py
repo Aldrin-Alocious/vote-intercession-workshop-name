49fa5b28bfb6e1a2a31e9edc0060cf860adfe3d7
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2023-02-08 00:10:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ elif op1=='Retail Trade':
 tab2.write("Communication Services")
 op2=st.selectbox('Pick one', ['Post', 'Mobile', 'Television', 'Dish'])
 if op2=='Post':
-  tab2.image('cd4da6f1-bb09-4e25-a203-48565df9a723.jpg')
+  tab2.image('52d4f12c-a36b-4346-ab91-531882e74196.jpg')
 elif op2=='Mobile':
   tab2.image('3719326c-f43f-417a-b6f1-1c4fe70ec865.jpg')
 elif op2=='Television':

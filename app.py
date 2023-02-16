6f8e7a89d16e1438c49f0c9f17d88eb3a7c7a5ef
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2023-02-16 16:48:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ tab1, tab2 = st.tabs(["Vote", "Results"])
 tab1.subheader("Holy Spirit guide us")
 with tab1.form("my_form",clear_on_submit=True):
   pc=st.text_input('Pass code')
-  op1=tab1.radio('Pick one', ['Connect', 'Bridge', 'Kneel', 'Entreaty'])
+  op1=st.radio('Pick one', ['Connect', 'Bridge', 'Kneel', 'Entreaty'])
   if op1=='Connect':
-    tab1.write('Intercession connects us with God. So we are actually connecting God while praying. And the workshop is all about helping people to stay in this connection./n/"Behold, I stand at the door, and knock: if any man hear my voice, and open the door, I will come in to him, and will sup with him, and he with me./"/nRevelation 3:20')
+    st.write('Intercession connects us with God. So we are actually connecting God while praying. And the workshop is all about helping people to stay in this connection./n/"Behold, I stand at the door, and knock: if any man hear my voice, and open the door, I will come in to him, and will sup with him, and he with me./"/nRevelation 3:20')
   elif op1=='Bridge':
-    tab1.write('Intercession is a bridge between those who pray and the God. So those who pray is actually building a bridge to God and there is a two way transportation of information (intentions, messages, grace, etc.)/n/"Your Love is the Bridge,/nYou build with a Cross./"/n/nThe workshop is about teaching them to break all barriers, and build bridges through intercession')
+    st.write('Intercession is a bridge between those who pray and the God. So those who pray is actually building a bridge to God and there is a two way transportation of information (intentions, messages, grace, etc.)/n/"Your Love is the Bridge,/nYou build with a Cross./"/n/nThe workshop is about teaching them to break all barriers, and build bridges through intercession')
   elif op1=='Kneel':
-    tab1.write('Know Now Eesho Earned Lives. We should pray because we belong to Him.')
+    st.write('Know Now Eesho Earned Lives. We should pray because we belong to Him.')
   elif op1=='Entreaty':
-    tab1.write('We should entreat to Jesus to stand in prayer life without considering the barriers in it. Entreaty means an earnest or humble request.')
+    st.write('We should entreat to Jesus to stand in prayer life without considering the barriers in it. Entreaty means an earnest or humble request.')
   if submitted:
     for i in range(0,10):
       if pc==st.session_state['secretcode']:

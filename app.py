4d9faba6ab1cda86750458465d47c4cd424b25e2
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2023-02-16 16:57:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,21 @@ if "voted" not in st.session_state:
   st.session_state.voted=[]
 if "publish" not in st.session_state:
   st.session_state.publish=False
+if "opt" not in st.session_state:
+  st.session_state.opt=''
 st.title('Intercession Workshop 2023')
 tab1, tab2 = st.tabs(["Vote", "Results"])
 tab1.subheader("Holy Spirit guide us")
 with tab1.form("my_form",clear_on_submit=True):
   pc=st.text_input('Pass code')
-  op1=st.radio('Pick one', ['Bridge', 'Connect', 'Kneel', 'Entreaty'])
-  if op1=='Connect':
+  st.session_state['opt']=st.radio('Pick one', ['Connect', 'Bridge', 'Kneel', 'Entreaty'])
+  if st.session_state['opt']=='Connect':
     st.write('Intercession connects us with God. So we are actually connecting God while praying. And the workshop is all about helping people to stay in this connection. "Behold, I stand at the door, and knock: if any man hear my voice, and open the door, I will come in to him, and will sup with him, and he with me." Revelation 3:20')
-  elif op1=='Bridge':
+  elif st.session_state['opt']=='Bridge':
     st.write('Intercession is a bridge between those who pray and the God. So those who pray is actually building a bridge to God and there is a two way transportation of information (intentions, messages, grace, etc.) "Your Love is the Bridge, You build with a Cross." The workshop is about teaching them to break all barriers, and build bridges through intercession')
-  elif op1=='Kneel':
+  elif st.session_state['opt']=='Kneel':
     st.write('Know Now Eesho Earned Lives. We should pray because we belong to Him.')
-  elif op1=='Entreaty':
+  elif st.session_state['opt']=='Entreaty':
     st.write('We should entreat to Jesus to stand in prayer life without considering the barriers in it. Entreaty means an earnest or humble request.')
   submitted=st.form_submit_button(label='Submit')
   if submitted:

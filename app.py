cb8f7b54f1ca1bd3e0aaf83fda38a4a41c448ce8
Aldrin-Alocious <99838573+Aldrin-Alocious@users.noreply.github.com>
2023-02-16 16:07:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 import streamlit as st
-st.title('TTC Assignment')
-tab1, tab2 = st.tabs(["Internal Trade", "Communication Services"])
-tab1.subheader("Internal Trade")
-op1=tab1.radio('Pick one', ['Whole Sale Trade', 'Retail Trade'])
-if op1=='Whole Sale Trade':
-  tab1.image('cd4da6f1-bb09-4e25-a203-48565df9a723.jpg')
-elif op1=='Retail Trade':
-  tab1.image('3719326c-f43f-417a-b6f1-1c4fe70ec865.jpg')
-tab2.subheader("Communication Services")
-op2=tab2.radio('Pick one', ['Post', 'Mobile', 'Television', 'Dish'])
-if op2=='Post':
-  tab2.image('52d4f12c-a36b-4346-ab91-531882e74196.jpg')
-elif op2=='Mobile':
-  tab2.image('53e1e72b-4c70-459a-8092-0ce2a2e18e10.jpg')
-elif op2=='Television':
-  tab2.image('40b399fd-bf64-49b8-9cc9-edb546e6405a.jpg')
-elif op2=='Dish':
-  tab2.image('b10217ce-310c-4e30-916b-80211835448c.jpg')
+st.title('Intercession Workshop 2023')
+tab1, tab2 = st.tabs(["Vote", "Results"])
+tab1.subheader("Holy Spirit guide us")
+op1=tab1.radio('Pick one', ['Connect', 'Bridge', 'Kneel', 'Entreaty'])
+if op1=='Connect':
+  tab1.write('Intercession connects us with God. So we are actually connecting God while praying. And the workshop is all about helping people to stay in this connection./n/"Behold, I stand at the door, and knock: if any man hear my voice, and open the door, I will come in to him, and will sup with him, and he with me./"/nRevelation 3:20')
+elif op1=='Bridge':
+  tab1.write('Intercession is a bridge between those who pray and the God. So those who pray is actually building a bridge to God and there is a two way transportation of information (intentions, messages, grace, etc.)/n/"Your Love is the Bridge,/nYou build with a Cross./"/n/nThe workshop is about teaching them to break all barriers, and build bridges through intercession')
+elif op1=='Kneel':
+  tab1.write('Know Now Eesho Earned Lives. We should pray because we belong to Him.')
+elif op1=='Entreaty':
+  tab1.write('We should entreat to Jesus to stand in prayer life without considering the barriers in it. Entreaty means an earnest or humble request.')
+tab2.subheader("Results will be published here")
